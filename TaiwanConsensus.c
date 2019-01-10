b7d319b0e6aef2f98a1507c4ca95fe057b2a9b09
snowcookie <snow.shanalike@gmail.com>
2019-01-10 18:10:50 +0800

    update equal method, update announce

--- TaiwanConsensus.c
+++ TaiwanConsensus.c
@@ -1,20 +1,20 @@
 #define TAIWAN_CONSENSUS_BUILDER(num_countries, num_systems) \
 	ANNOUNCE(num_countries country num_systems system), \
 	BOTH(IS_ONE_COUNTRY(num_countries), IS_TWO_SYSTEM(num_systems)) ? \
-		ANNOUNCE(Taiwan is not part of china.), ANNOUNCE(No such thing. Look at HK.) : \
+		ANNOUNCE(No such thing. Look at HK.) : \
 	IS_ONE_COUNTRY(num_countries) ? \
 		ANNOUNCE(Taiwan is not part of china.) : \
 	BOTH(IS_TWO_COUNTRY(num_countries), IS_TWO_SYSTEM(num_systems)) ? \
 		ANNOUNCE(Taiwan Consensus.) : \
-	ANNOUNCE(Are you funcking kidding?), \
+	ANNOUNCE(Funcking kidding.), \
 	ANNOUNCE()
 
-#define IS_ONE_COUNTRY(num_countries) (num_countries == 1)
-#define IS_TWO_COUNTRY(num_countries) (num_countries == 2)
-#define IS_TWO_SYSTEM(num_systems)    (num_systems   == 2)
+#define IS_ONE_COUNTRY(num_countries) !(num_countries ^ 1)
+#define IS_TWO_COUNTRY(num_countries) !(num_countries ^ 2)
+#define IS_TWO_SYSTEM(num_systems)    !(num_systems   ^ 2)
 
 #define ANNOUNCE(x) puts(#x)
-#define BOTH(x, y) ((x) && (y))
+#define BOTH(x, y) ((x) & (y))
 
 int puts(const char *);
 
